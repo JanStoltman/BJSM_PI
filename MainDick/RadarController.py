22cdd181724ea038571d877595f65611bd7f7d2e
sgorawski <sgorawski@gmail.com>
2016-12-16 23:09:24 +0100

    kwadratowy radar

--- MainDick/RadarController.py
+++ MainDick/RadarController.py
@@ -1,10 +1,27 @@
-
+from MainDick.SpaceObjects.Planet import Planet
 
 class RadarController:
 
-    def scan_for_planets(self, planets, radar_radius, ship_position, space_width, space_height):
-        search_square = self.convert_to_square(radar_radius, ship_position, space_width, space_height)
+    def scan_for_planets(self, planets, radar_radius, ship_position_x, ship_position_y, space_width, space_height, use_square_first = True):
+        search_square = self.convert_to_square(radar_radius, ship_position_x, ship_position_y, space_width, space_height)
+        planets_to_scan_for = self.planets_in_square(planets, search_square) if use_square_first else planets
 
 
-    def convert_to_square(self, half_side, center, max_x, max_y):
-        left_lower_x = center[0] - side if center[0] - side >= 0 else 0
+
+
+    def convert_to_square(self, half_side, center_x, center_y, max_x, max_y):
+        left_lower_x = center_x - half_side if center_x - half_side >= 0 else 0
+        left_lower_y = center_y - half_side if center_y - half_side >= 0 else 0
+        right_upper_x = center_x + half_side if center_x + half_side <= max_x else max_x
+        right_upper_y = center_y + half_side if center_y + half_side <= max_y else max_y
+
+        return ((left_lower_x, left_lower_y), (right_upper_x, right_upper_y))
+
+
+    def planets_in_square(self, planets, square):
+        list_of_planets_in_square = []
+        for planet in planets:
+            if planet.coordinates[0] in range(square[0][0], square[0][1]) and planet.coordinates[1] in range(square[1][0], square[1][1]):
+                list_of_planets_in_square.append(planet)
+
+        return list_of_planets_in_square
