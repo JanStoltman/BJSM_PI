fe2f1316cca73ca0bc2f64a216428c48005533b6
DESKTOP-ASE44EL\Kubalus <kubalka111@gmail.com>
2016-12-16 20:54:31 +0100

    dodaje statek

---
+++ MainDick/SpaceObjects/Spacecraft.py
@@ -0,0 +1,9 @@
+class Spacecraft
+    def __init__(name, self, power, fuel, mass, position_x, position_y, image):
+        self.name = name
+        self.power = power
+        self.fuel = fuel
+        self.mass = mass
+        self.position_x = position_x
+        self.position_x = position_x
+        self.image = image
