395449eb5c128c4fb56c0f07ce03fa1ced866f14
sgorawski <sgorawski@gmail.com>
2016-12-16 21:51:40 +0100

    cd commita

--- MainDick/PlanetsArrangementController.py
+++ MainDick/PlanetsArrangementController.py
@@ -40,6 +40,7 @@ class PlanetsArrangementController:
 
         return list_of_planets
 
+
     def acceptable_coordinates(self, list_of_coords, curr_planet_coords, min_distance):
 
         curr_x, curr_y = curr_planet_coords
