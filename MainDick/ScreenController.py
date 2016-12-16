b21f0fb90c9763dacc88e4d0f5e6f0199686f305
JanStoltman <stoltmanjan@gmail.com>
2016-12-16 20:29:15 +0100

    Space Up

--- MainDick/ScreenController.py
+++ MainDick/ScreenController.py
@@ -6,9 +6,11 @@ class ScreenController:
         self.screen = tkinter.Tk()
         self.screen.update()
         self.screen.geometry("{0}x{1}+0+0".format(
-            self.screen.winfo_screenwidth() - 100,  self.screen.winfo_screenheight() - 100))
-        self.width = self.screen.winfo_width()
-        self.height = self.screen.winfo_height()
+            self.screen.winfo_screenwidth(),  self.screen.winfo_screenheight()))
+        self.width = self.screen.winfo_screenwidth()
+        self.height = self.screen.winfo_screenheight()
+        self.canvas = tkinter.Canvas(self.screen, bg="black", height=self.height, width=self.width)
+        self.canvas.pack()
 
     def show_screen(self):
         self.screen.mainloop()
