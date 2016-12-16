d5ec460f74a52d141783489e183e9d64b698cac4
JanStoltman <stoltmanjan@gmail.com>
2016-12-16 21:36:48 +0100

    Space added

--- MainDick/SpaceObjects/Spacecraft.py
+++ MainDick/SpaceObjects/Spacecraft.py
@@ -1,9 +1,9 @@
-class Spacecraft
-    def __init__(name, self, power, fuel, mass, position_x, position_y, image):
+class Spacecraft:
+    def __init__(self, name, power, fuel, mass, position_x, position_y, image):
         self.name = name
         self.power = power
         self.fuel = fuel
         self.mass = mass
         self.position_x = position_x
-        self.position_x = position_x
+        self.position_y = position_y
         self.image = image
--- MainDick/main.py
+++ MainDick/main.py
@@ -1,4 +1,19 @@
 from MainDick.ScreenController import ScreenController
+from MainDick.SpaceObjects.Spacecraft import Spacecraft
+from MainDick.PlanetsArrangementController import PlanetsArrangementController
 
-screenController = ScreenController.ScreenController()
+screenController = ScreenController()
+planets = PlanetsArrangementController().arrange_planets(number_of_planets=3, colors_list=["blue", "red", "green"],
+                                                         minimal_distance=10,
+                                                         min_mass=10,
+                                                         min_radius=5,
+                                                         max_mass=100,
+                                                         max_radius=20,
+                                                         max_height=screenController.height,
+                                                         max_width=screenController.width,
+                                                         margin=10)
+
+craft = Spacecraft(fuel=100, power=10, mass=10, position_y=50, position_x=50, image="Spacecraft.png",name="Dupa")
+
+screenController.pack_canvas(planets, craft)
 screenController.show_screen()
