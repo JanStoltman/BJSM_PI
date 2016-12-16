6d3a20e673c77020bce293f62992dd883b723a67
JanStoltman <stoltmanjan@gmail.com>
2016-12-16 23:41:59 +0100

    ImageLoader

--- MainDick/main.py
+++ MainDick/main.py
@@ -2,9 +2,10 @@ from MainDick.ScreenController import ScreenController
 from MainDick.SpaceObjects.Spacecraft import Spacecraft
 from MainDick.PlanetsArrangementController import PlanetsArrangementController
 
+import MainDick.ImageLoader as ImL
+
 screenController = ScreenController()
 planets = PlanetsArrangementController().arrange_planets(number_of_planets=3,
-                                                         colors_list=["#32cd32", "#ffd700", "#bc8f8f"],
                                                          minimal_distance=300,
                                                          min_mass=10,
                                                          min_radius=5,
@@ -13,10 +14,10 @@ planets = PlanetsArrangementController().arrange_planets(number_of_planets=3,
                                                          max_height=screenController.height,
                                                          max_width=screenController.width,
                                                          margin=10,
-                                                         files_list=["planet0.png", "planet1.png"])
+                                                         files_list=ImL.get_planet_images())
 craft = Spacecraft(fuel=100, power=10, mass=10,
                    position_x=screenController.width, position_y=screenController.height,
-                   image="Spacecraft.png", name="Dupa")
+                   image=ImL.get_spacecraft_image(), name="Dupa")
 
 screenController.pack_canvas(planets, craft)
 screenController.show_screen()
