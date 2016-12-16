2dafcdd0ddef01a5589e30c674e8628e15ed8dce
Mateozaur <mat9913@gmail.com>
2016-12-16 23:08:57 +0100

    no message

--- MainDick/ForceController.py
+++ MainDick/ForceController.py
@@ -1,7 +1,7 @@
 import MainDick.Vectors.vectors as vectors
 
 def proximity(ship, planet):
-    return ((ship.coordinates[0]-planet.coordinates[0])**2+(ship.coordinates[1]-planet.coordinates[1])**2)**(1/2)
+    return ((ship.position_x-planet.coordinates[0])**2+(ship.position_y-planet.coordinates[1])**2)**(1/2)
 def gravity(ship, planet):
     return 6.67*planet.mass()*ship.mass()/proximity(ship.coords(), planet)**2
 def grav_vector(ship, planet):
@@ -14,5 +14,5 @@ def net_move(gravs, thrust, tick=1):
         net.sum(v.multiply(tick**2/2))
     return net.sum(thrust.multiply(tick))
 def movement(net_r, ship):
-    ship.coordinates = ship.coordinates[0]+net_r[0], ship.coordinates[1]+net_r[1], 0
+    ship.coordinates = ship.position_x+net_r[0], ship.position_y+net_r[1], 0
 
