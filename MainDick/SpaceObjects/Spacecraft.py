ac38803124db4dcb12f8b1afe3c15f9c60b31868
Mateozaur <mat9913@gmail.com>
2016-12-16 22:51:24 +0100

    Dodałem 3cią współrzędną - nie używana, ale potrzebna dla vectors

--- MainDick/SpaceObjects/Spacecraft.py
+++ MainDick/SpaceObjects/Spacecraft.py
@@ -1,10 +1,11 @@
 class Spacecraft:
-    def __init__(self, name, power, fuel, mass, position_x, position_y, image, direction = 0):
+    def __init__(self, name, power, fuel, mass, position_x, position_y, image, direction = 0, position_z = 0):
         self.name = name
         self.power = power
         self.fuel = fuel
         self.mass = mass
         self.position_x = position_x
         self.position_y = position_y
+        self.position_z = position_z
         self.image = image
         self.direction = direction
