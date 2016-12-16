c9e8e49e417943d7e13fe7017087179a76861b3c
sgorawski <sgorawski@gmail.com>
2016-12-16 20:11:23 +0100

    skonczono planety

--- MainDick/Planet.py
+++ MainDick/Planet.py
@@ -3,3 +3,6 @@ class Planet:
     def __init__(self, name, mass, radius, x, y):
         self.name = name
         self.mass = mass
+        self.radius = radius
+        self.x = x
+        self.y = y
