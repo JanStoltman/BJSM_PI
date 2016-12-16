8b1a87fc45a630dfc6edb6fca77fc9948153f6d3
sgorawski <sgorawski@gmail.com>
2016-12-16 21:16:36 +0100

    prawie planets arr cont

--- MainDick/PlanetsArrangementController.py
+++ MainDick/PlanetsArrangementController.py
@@ -3,7 +3,7 @@ import random
 
 class PlanetsArrangementController:
 
-    def arrange_planets(self, number, colors, minimal_distance, min_mass, max_mass, min_radius, max_radius,
+    def arrange_planets(self, number_of_planets, colors_list, minimal_distance, min_mass, max_mass, min_radius, max_radius,
                         max_width, max_height, margin):
 
         if max_width < margin or max_height < margin:
@@ -11,14 +11,23 @@ class PlanetsArrangementController:
 
         list_of_planets = []
 
-        if number >= 1:
+        if number_of_planets >= 1:
             first_planet = Planet('planet0',
-                                  random.choice(colors),
+                                  random.choice(colors_list),
                                   random.randint(min_mass, max_mass),
                                   random.randint(min_radius, max_radius),
                                   (random.randint(margin, max_width - margin), random.randint(margin, max_height - margin)))
 
             list_of_planets.append(first_planet)
 
-        for _ in range(1, number):
+        for i in range(1, number_of_planets):
+            next_planet = Planet('planet' + str(i),
+                                  random.choice(colors_list),
+                                  random.randint(min_mass, max_mass),
+                                  random.randint(min_radius, max_radius),
+                                  (random.randint(margin, max_width - margin), random.randint(margin, max_height - margin)))
+
+            list_of_planets.append(next_planet)
+
+        return list_of_planets
 
