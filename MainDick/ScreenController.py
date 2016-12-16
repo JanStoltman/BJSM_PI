3a1edbd8576ff49791c5c44a311cb8a8aba21178
JanStoltman <stoltmanjan@gmail.com>
2016-12-16 21:49:40 +0100

    asdasd

--- MainDick/ScreenController.py
+++ MainDick/ScreenController.py
@@ -12,7 +12,8 @@ class ScreenController:
         self.width = self.screen.winfo_screenwidth()
         self.height = self.screen.winfo_screenheight()
         self.spacecraft_bitmap = None
-        self.plantes = None
+        self.planets = None
+        self.filename = None
         self.canvas = tkinter.Canvas(self.screen, bg="black", height=self.height, width=self.width)
 
     def show_screen(self):
@@ -27,10 +28,12 @@ class ScreenController:
                                     fill=p.color)
 
     def add_spacecraft(self, spacecraft):
-        filename = tkinter.PhotoImage(file=spacecraft.image)
-        self.spacecraft_bitmap = self.canvas.create_image(spacecraft.position_x, spacecraft.position_y, image=filename)
+        self.filename = tkinter.PhotoImage(file=spacecraft.image)
+        self.spacecraft_bitmap = self.canvas.create_image((spacecraft.position_x, spacecraft.position_y),
+                                                          image=self.filename)
 
     def pack_canvas(self, _planets, _spacecraft):
+        self.canvas.pack()
         self.add_planet(planets=_planets)
         self.add_spacecraft(spacecraft=_spacecraft)
-        self.canvas.pack()
+
