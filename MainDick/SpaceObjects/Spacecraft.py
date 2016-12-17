dc39b7e1469f1b47bb69a4c3e2f79cac2ebf1f3c
sgorawski <sgorawski@gmail.com>
2016-12-17 11:05:30 +0100

    Work in progress

--- MainDick/SpaceObjects/Spacecraft.py
+++ MainDick/SpaceObjects/Spacecraft.py
@@ -1,13 +1,34 @@
+from MainDick.ForceController import ForceController
+from MainDick.Vector import Vector
+
 class Spacecraft:
-    def __init__(self, name, power, fuel, mass, position, image, base_station, speed = 20, direction = 0,radius = 35):
+
+
+    def __init__(self, name, power, fuel, mass, position, image, direction = 0,radius = 35):
+
         self.name = name
         self.power = power
         self.fuel = fuel
         self.mass = mass
         self.position = position
         self.image = image
-        self.base_station = base_station
-        self.speed = speed
         self.direction = direction
         self.radius = radius
+
+
+    def fly(self, planets, direction):
+
+        gravity_vector = ForceController().calculate_gravity_vector(planets, self)
+
+        if direction == 'fd':
+            thrust_vector = Vector().from_len_and_angle(len, angle)
+        elif direction == 'lt':
+
+        elif direction == 'rt':
+
+        else:
+            raise ValueError('Wrong direction')
+
+
+
 
