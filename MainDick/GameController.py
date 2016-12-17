416a1809e750ab349103b2afeb8b4b2e2480ca1b
sgorawski <sgorawski@gmail.com>
2016-12-17 01:51:02 +0100

    dodano warunki wygranej i przegranej

--- MainDick/GameController.py
+++ MainDick/GameController.py
@@ -1,24 +1,35 @@
-from MainDick.Point import Point
-
 class GameController:
 
     def flight(self, planets, ship, max_x, max_y):
 
 
 
-        ship.position_x = ship.position_x + 10
-        ship.position_y = ship.position_y + 20
+        ship.position.x = ship.position.x + 10
+        ship.position.y = ship.position.y + 20
         ship.direction = (ship.direction + 10) % 360
 
 
     def is_dead(self, planets, ship):
 
-        return is_out_of_space(planets, ship) or crashed(planets, ship)
+        return self.is_out_of_space(ship.position) or self.crashed(planets, ship.position)
 
 
-    def reached_destination(self, ship, destination):
+    def reached_destination(self, ship_position, destination):
 
-        return
+        return ship_position.proximity(destination.coordinates) <= destination.radius
+
+
+    def is_out_of_space(self, ship_position, max_x, max_y):
+
+        return not ship_position.x in range(0, max_x) and ship_position.y in range(0, max_y)
+
+
+    def crashed(self, planets, ship_position):
+
+        for planet in planets:
+            if self.reached_destination(ship_position, planet):
+                return True
+        return False
 
 
 
