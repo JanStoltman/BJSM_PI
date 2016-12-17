7eea8b9f3087d3574cea1a27bc9c9b0aaa944cfe
sgorawski <sgorawski@gmail.com>
2016-12-17 11:28:31 +0100

    spacecraft cd

--- MainDick/SpaceObjects/Spacecraft.py
+++ MainDick/SpaceObjects/Spacecraft.py
@@ -31,14 +31,14 @@ class Spacecraft:
             thrust_vector = thrust_vector.sum(self.vector_from_len_and_angle(self.power, self.direction))
 
         if 'bk' in directions:
-            thrust_vector = thrust_vector.sum(self.vector_from_len_and_angle(self.power, self.direction + 180))
+            thrust_vector = thrust_vector.sum(self.vector_from_len_and_angle(self.power / 2, self.direction + 180))
 
-
+        movement_vector = gravity_vector.sum(thrust_vector)
 
         self.position.x += movement_vector.X
         self.position.y += movement_vector.Y
 
-        return gravity_vector.sum(thrust_vector)
+        return movement_vector.to_list()
 
 
     def vector_from_len_and_angle(self, len, angle):
