32a9523b1c7c0d81fb80d5c5db94c0c2d7ed1bd4
sgorawski <sgorawski@gmail.com>
2016-12-17 11:17:56 +0100

    wyrzuciłem paliwo

--- MainDick/SpaceObjects/Spacecraft.py
+++ MainDick/SpaceObjects/Spacecraft.py
@@ -5,11 +5,10 @@ from math import radians, cos, sin
 class Spacecraft:
 
 
-    def __init__(self, name, power, fuel, mass, position, image, direction = 0,radius = 35):
+    def __init__(self, name, power, mass, position, image, direction = 0,radius = 35):
 
         self.name = name
         self.power = power
-        self.fuel = fuel
         self.mass = mass
         self.position = position
         self.image = image
