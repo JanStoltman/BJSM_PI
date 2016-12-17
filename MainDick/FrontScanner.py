ac95781e357fcd41ab46175a17880546145f93f4
JanStoltman <stoltmanjan@gmail.com>
2016-12-17 10:24:13 +0100

    Merge

--- MainDick/FrontScanner.py
+++ MainDick/FrontScanner.py
@@ -3,9 +3,9 @@ import math
 
 class FrontScanner:
 
-    def will_collide(self, ship, planet_candidates):
-        a = math.tan(math.pi + ma math.radians(ship.direction))
-        b = ship.position.y - a * ship.position.x
+    def will_collide(self, ship, planet_candidates, max_width, max_height):
+        a = math.tan(math.pi + math.radians(ship.direction))
+        b = math.fabs(max_height-ship.position.y)- a * math.fabs(max_width-ship.position.x)
         print(a, b, ship.direction, ship.position.x, ship.position.y)
         for planet in planet_candidates:
             prox = abs(a * math.fabs(max_width-planet.coordinates.x) - math.fabs(max_height-planet.coordinates.y) + b) / math.sqrt((a ** 2) + 1)
