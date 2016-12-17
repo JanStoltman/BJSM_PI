644abc70affa2f3987e20060d9cd934995d4d54d
Mateozaur <mat9913@gmail.com>
2016-12-17 05:14:36 +0100

    FrontScanner

---
+++ MainDick/FrontScanner.py
@@ -0,0 +1,19 @@
+from MainDick.Point import Point
+from MainDick.SpaceObjects.Spacecraft import Spacecraft
+from math import tan, pi
+class FrontScanner:
+
+    def will_collide(self, ship, planets):
+        a = tan((ship.direction*pi/180)+pi/2)
+        b = ship.position.y - a*ship.position.x
+        for planet in planets:
+            prox = abs(a*planet.coordinates.x-planet.coordinates.y+b)/(((a**2)+1)**(1/2))
+            if prox <= planet.radius:
+                return True
+        return False
+
+
+
+
+
+
