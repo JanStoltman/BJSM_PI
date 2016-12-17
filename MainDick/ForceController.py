45e50fb92912055499d8cb9b333abc73cbf6c8cb
sgorawski <sgorawski@gmail.com>
2016-12-17 03:45:23 +0100

    forcecontroller v 3.0

--- MainDick/ForceController.py
+++ MainDick/ForceController.py
@@ -4,35 +4,31 @@ from MainDick.SpaceObjects.Planet import Planet
 from MainDick.Point import Point
 from MainDick.Vector import Vector
 
-def gravity(ship, planet):
-    return 6.67*planet.mass*ship.mass/ship.position.proximity(planet.coordinates)**2
+class ForceController:
+
+    def gravity(self, ship, planet):
+        G = 6.67 * 10 ** -11
+        return G * planet.mass * ship.mass / ship.position.proximity(planet.coordinates) ** 2
 
 
-def from_points(point1, point2):
-    return Vector(point2.x - point1.x, point2.y - point1.y)
-
-def grav_vector(ship, planet):
-    v = from_points(ship.position, planet.coordinates)
-    v = v.multiply(gravity(ship, planet)/v.magnitude())
-    return v
+    def from_points(self, point1, point2):
+        return Vector(point2.x - point1.x, point2.y - point1.y)
 
 
-def net_move(gravs):
-    net = Vector(0, 0)
-    for v in gravs:
-        print(v.to_list())
-        print(net.to_list())
-        net = net.sum(v)
-        print(net.to_list())
-    return net
+    def grav_vector(self, ship, planet):
+        v = self.from_points(ship.position, planet.coordinates)
+        v = v.multiply(self.gravity(ship, planet) / v.magnitude())
+        return v
 
-"""
-def movement(net_r, ship):
-    ship.position.x = ship.position.x+net_r.X
-    ship.position.y = ship.position.y+net_r.Y
-"""
 
-statek = Spacecraft('stateczek', 10, 10, 10, Point(1, 1), get_spacecraft_image())
-planeta = Planet('Jebudu', 10, 10, Point(5, 4), get_spacecraft_image())
-vec = grav_vector(statek,planeta)
-print(net_move([vec]))
+    def net_gravity(self, gravs):
+        net = Vector(0, 0)
+        for v in gravs:
+            net = net.sum(v)
+        return net
+
+
+    def calculate_movement_vector(self, planets, ship):
+
+
+
