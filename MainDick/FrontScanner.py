2814fc030e43a17dd227f684969536c8bcbe3993
JanStoltman <stoltmanjan@gmail.com>
2016-12-17 10:34:13 +0100

    Up front

--- MainDick/FrontScanner.py
+++ MainDick/FrontScanner.py
@@ -2,14 +2,10 @@ import math
 
 
 class FrontScanner:
-
     def will_collide(self, ship, planet_candidates, max_width, max_height):
-        a = math.tan(math.pi + math.radians(ship.direction))
-        b = math.fabs(max_height-ship.position.y)- a * math.fabs(max_width-ship.position.x)
-        print(a, b, ship.direction, ship.position.x, ship.position.y)
         for planet in planet_candidates:
-            prox = abs(a * math.fabs(max_width-planet.coordinates.x) - math.fabs(max_height-planet.coordinates.y) + b) / math.sqrt((a ** 2) + 1)
-            if prox <= (planet.radius + ship.radius) and self.plausible_coords(ship.direction, ship.position, planet.coordinates):
+            if planet.mass > 0 and math.hypot(planet.coordinates.x - ship.position.x,
+                          planet.coordinates.y - ship.position.y) <= planet.radius + (ship.radius * (math.fabs(ship.speed) / 10 + 1)):
                 return True
         return False
 
@@ -22,9 +18,3 @@ class FrontScanner:
             return planet_coords.x < ship_pos.x and planet_coords.y >= ship_pos.y
         else:
             return planet_coords.x < ship_pos.x and planet_coords.y < ship_pos.y
-
-
-
-
-
-
