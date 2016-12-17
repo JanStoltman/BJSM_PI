1713549aa270261d5e1c9766be89084624cd112b
Mateozaur <mat9913@gmail.com>
2016-12-17 02:46:41 +0100

    no message

--- MainDick/ForceController.py
+++ MainDick/ForceController.py
@@ -1,21 +1,30 @@
 import MainDick.Vectors.vectors as vectors
+from MainDick.ImageLoader import get_spacecraft_image
+from MainDick.SpaceObjects.Spacecraft import Spacecraft
+from MainDick.SpaceObjects.Planet import Planet
+from MainDick.Point import Point
 
-def proximity(ship, planet):
-    return ((ship.position_x-planet.coordinates[0])**2+(ship.position_y-planet.coordinates[1])**2)**(1/2)
 def gravity(ship, planet):
-    return 6.67*planet.mass()*ship.mass()/proximity(ship, planet)**2
+    return 6.67*planet.mass*ship.mass/ship.position.proximity(planet.coordinates)**2
 def grav_vector(ship, planet):
-    ship_coordinates = vectors.Point(ship.position_x, ship.position_y, ship.position_z)
-    planet_coordinates = vectors.Point(planet.coordinates[0], planet.coordinates[1], planet.coordinates[2])
+    ship_coordinates = vectors.Point(ship.position.x, ship.position.y, ship.position.z)
+    planet_coordinates = vectors.Point(planet.coordinates.x, planet.coordinates.y, planet.coordinates.z)
     v = vectors.Vector.from_points(ship_coordinates, planet_coordinates)
-    v.multiply(gravity(ship, planet)/v.magnitude())
+    v = v.multiply(gravity(ship, planet)/v.magnitude())
     return v
-def net_move(gravs, thrust, tick=1):
+def net_move(gravs):
     net = vectors.Vector(0,0,0)
     for v in gravs:
-        net.sum(v.multiply(tick**2/2))
-    return net.sum(thrust.multiply(tick))
+        print(v.vector)
+        print(net.vector)
+        net = net.sum(v)
+        print(net.vector)
+    return net
 def movement(net_r, ship):
-    ship.position_x = ship.position_x+net_r[0]
-    ship.position_y = ship.position_y+net_r[1]
+    ship.position.x = ship.position.x+net_r.vector[0]
+    ship.position.y = ship.position.y+net_r.vector[1]
 
+statek = Spacecraft('stateczek', 10, 10, 10, Point(1, 1), get_spacecraft_image())
+planeta = Planet('Jebudu', 10, 10, Point(5,4), get_spacecraft_image())
+vec = grav_vector(statek,planeta)
+print(net_move([vec]))
